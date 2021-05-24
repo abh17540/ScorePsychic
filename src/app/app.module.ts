e573d352b5e8f4b1345b5723623ce5d8f0bc8364
Abhi Sharma <sharm395@purdue.edu>
2021-05-24 17:57:40 -0400

    Circular animation created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MatchPredictionComponent } from './match-prediction/match-prediction.component';
+import { PredictionRingComponent } from './prediction-ring/prediction-ring.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatSliderModule} from '@angular/material/slider';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchPredictionComponent,
+    PredictionRingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
